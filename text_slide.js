3e59a65db795b6161b597e6de48a8edba731879c
minefled <69519454+minefled@users.noreply.github.com>
2021-03-19 14:46:37 +0100

    added some comments to text_slide.js

--- text_slide.js
+++ text_slide.js
@@ -10,17 +10,21 @@ class TextSlider {
     _distance = 200;
 
     constructor(ele_id, options) {
+        // load options
         this.options = options;
         this._load_options(options);
 
+        // set ele_id
         this.ele_id = ele_id;
 
+        // set basic style of the element
         this.getEle().style.display = "flex";
         this.getEle().style.flexDirection = "row";
         this.getEle().style.overflow = "hidden";
     }
 
     _load_options(options) {
+        // load options from the options object
         if(options.hasOwnProperty("delay")) {this._delay = options.delay;}
         if(options.hasOwnProperty("spacewidth")) {this._spacewidth = options.spacewidth;}
         if(options.hasOwnProperty("distance")) {this._distance = options.distance;}
@@ -28,14 +32,20 @@ class TextSlider {
     }
 
     showText(content) {
+        // delete everything first
         this.deleteAll();
+        // then wait until it's done
         setTimeout(() => {
+            // create an empty list where all the newly created elements will be temporarily saved
             var elements = [];
-
+            
+            // go through the text and create divs
+            // for each character with some styles
             for(var i=0;i<content.length;i++) {
                 var new_char = document.createElement("div");
                 new_char.innerHTML = content[i];
 
+                // style
                 new_char.style.transition = this._char_transition;
                 new_char.style.opacity = "0%";
                 new_char.style.height = "100%";
@@ -43,11 +53,15 @@ class TextSlider {
                     new_char.style.width = this._spacewidth.toString() + "px";
                 }
                 new_char.style.transform = "translateY("+this._distance.toString()+"%)";
-
+                
+                // append to element and push to elements
                 this.getEle().appendChild(new_char);
                 elements.push(new_char);
             }
+            
 
+            // go through all elements and fade them in width the delay definded in
+            // this._delay
             var index = 0;
             var max_index = elements.length;
 
@@ -66,11 +80,14 @@ class TextSlider {
         }, this.estimateTimeLeft());
     }
     deleteAll() {
+        // get all childnodes of the element
         var childNodes = this.getEle().childNodes;
 
         var index = 0;
         var max_index = childNodes.length;
 
+        // go through all childnodes and fade them out
+        // with the delay defined in this._delay
         function remove_next_char(object) {
             var ele = childNodes[index];
             if (ele) {
